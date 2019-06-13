48c592fb14df9d2f255c6a2f36db9a8004ec7c83
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-06-13 18:14:24 -0400

    button led code

---
+++ button_led.ino
@@ -0,0 +1,30 @@
+
+#define IR 2
+void setup() {
+   pinMode(IR,INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+}
+
+void loop() {
+ if(digitalRead(IR)==HIGH)
+ {digitalWrite(3,HIGH);
+ delay(1000);
+ digitalWrite(3,LOW);
+ delay(100);
+ digitalWrite(4,HIGH);
+ delay(1000);
+ digitalWrite(4,LOW);
+ delay(100);
+ digitalWrite(5,HIGH);
+ delay(1000);
+ digitalWrite(5,LOW);
+ delay(100);
+
+  
+ 
+ }
+ 
+}
