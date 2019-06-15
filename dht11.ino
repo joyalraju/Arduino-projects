f29360a5b6670006969620448090a3ad26bf11c3
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-06-15 18:05:14 -0400

    dht sensor interface

---
+++ dht11.ino
@@ -0,0 +1,14 @@
+#include<dht.h>
+dht DHT;
+#define DHT11_PIN 3
+void setup() {
+Serial.begin(9600);
+Serial.println("Temperature & Humidity Sensor"); }
+void loop() { // READ DATA
+int chk = DHT.read11(DHT11_PIN);
+Serial.println(" Humidity " );
+Serial.println(DHT.humidity, 1);
+Serial.println(" Temparature ");
+Serial.println(DHT.temperature, 1);
+delay(2000);
+}
