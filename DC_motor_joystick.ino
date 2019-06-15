ea0fcd4794b7a690ac11f67643a0938f7263a074
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-06-15 18:08:15 -0400

    dc motor joystick control

---
+++ DC_motor_joystick.ino
@@ -0,0 +1,17 @@
+#define A 8
+#define B 9
+
+int a,x,b,y;
+void setup() {
+  pinMode(A,OUTPUT);
+  pinMode(B,OUTPUT);
+  digitalWrite(A,LOW);
+}
+
+void loop() {
+ 
+  x = analogRead(A0);
+  b = map(x,0,1023,0,255);
+   analogWrite(B,b);
+  
+}
