3aa1ffc8d25ae59ff5c58f9ee737c01da8e56d40
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-06-23 17:18:06 -0400

    pir sensor interface

---
+++ pirsensor.ino
@@ -0,0 +1,28 @@
+
+void setup() {
+Serial.begin(9600);
+pinMode(A0,INPUT);
+pinMode(2,OUTPUT);
+}
+
+void loop() {
+
+  int x=analogRead(A0);
+  //int d=x*1024/5;
+  Serial.println(x);
+  if(x>500)
+    {digitalWrite(2,HIGH);
+      delay(100);
+    digitalWrite(2,LOW);
+      delay(500);
+    digitalWrite(2,HIGH);
+      delay(100);
+    digitalWrite(2,LOW);
+    delay(500);    
+    digitalWrite(2,HIGH);
+      delay(100);
+    digitalWrite(2,LOW);
+    delay(500);
+    } 
+
+}
