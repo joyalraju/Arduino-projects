ca171f548ce8e4f9ae741f89ee2c713830e83ce6
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-07-01 18:17:44 -0400

    serial print

---
+++ serialprint_variables.ino
@@ -0,0 +1,20 @@
+int x=0;
+float y=0.0;
+void setup() {
+  Serial.begin(9600);
+  
+  
+
+}
+
+void loop() {
+  x=x+1;
+  y=y+0.5;
+  Serial.print("x= ");
+   Serial.println(x);
+  delay(1000);
+   Serial.print("y= ");
+    Serial.print(y);
+  delay(1000);
+   
+}
