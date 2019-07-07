32a68a506409af2d69934e8dc86b10821bc77ea1
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-07-06 23:12:37 -0400

    servo motor

---
+++ servo.ino
@@ -0,0 +1,18 @@
+#include<Servo.h>
+
+Servo eee;
+int a,x;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  eee.attach(9);
+}
+
+void loop() {
+  x = analogRead(A0);
+  a = map(x,0,1023,0,179);
+  eee.write(a);
+  delay(10);
+  
+}
